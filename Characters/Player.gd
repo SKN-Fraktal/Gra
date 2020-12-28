5f5637608e8f69af78a17e27130a17b7f367c821
Jakub-Kolacz <76391585+Jakub-Kolacz@users.noreply.github.com>
2020-12-28 19:12:47 +0100

    Update Player.gd

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -1,27 +1,28 @@
 extends KinematicBody2D
 
 const UP = Vector2(0, -1)
-const SLOPE_STOP = 40
+const SLOPE_STOP = 40 # odpowiada za zatrzymanie na skosie
 
 var velocity = Vector2()
 var move_speed = 250
 var gravity = 500
 var jump_velocity = -350
-var is_grounded = is_on_floor() 
+var is_grounded
 
 func _get_input():
-	var move_direction = -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right"))
-	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2)
+	var move_direction = -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right")) #movement w lewo i w prawo
+	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2) #odpowiada za nadanie predkosci, ostatnia wartosc to przyspieszenie
 	if move_direction != 0:
-		$AnimatedSprite.scale.x = move_direction
+		$AnimatedSprite.scale.x = move_direction #kierunek animacji
 
 func _input(event):
-	if event.is_action_pressed("jump") && is_grounded:
+	if event.is_action_pressed("jump") && is_grounded: #skok
 		velocity.y = jump_velocity
 
 func _physics_process(delta):
 	_get_input()
-	velocity = move_and_slide(velocity, UP, SLOPE_STOP)
-	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, UP, SLOPE_STOP)  #predkosc postaci w grze
+	velocity.y += gravity * delta  #odpowiada za zaprogramowanie grawitacji
+	is_grounded = is_on_floor()
 	
 
