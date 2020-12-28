05b81f4c9742c01bcae38370070340f64e3590e5
Jakub-Kolacz <76391585+Jakub-Kolacz@users.noreply.github.com>
2020-12-28 21:57:50 +0100

    Update Player.gd

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -7,7 +7,13 @@ var velocity = Vector2()
 var move_speed = 250
 var gravity = 500
 var jump_velocity = -350
+var endofjump = 0
 var is_grounded
+var health = 100
+var lives = 3
+
+func _ready():
+	new_game()
 
 func _get_input():
 	var move_direction = -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right")) #movement w lewo i w prawo
@@ -17,15 +23,39 @@ func _get_input():
 
 func _input(event):
 	if event.is_action_pressed("jump") && is_grounded: #skok
+		endofjump = 0
 		velocity.y = jump_velocity
 	#odpowiada za wysokosc skoku
-	if event.is_action_released("jump"):
-		velocity.y = 8.33
+	if event.is_action_released("jump")&& !endofjump:
+		if velocity.y < 0:
+			velocity.y = 8.33
+		endofjump = 1
 
 func _physics_process(delta):
 	_get_input()
 	velocity = move_and_slide(velocity, UP, SLOPE_STOP)  #predkosc postaci w grze
 	velocity.y += gravity * delta  #odpowiada za zaprogramowanie grawitacji
 	is_grounded = is_on_floor()
+	
+	
+#nie wiem czy to sie tak robi, i jaki byl sygnal by to był ale domyslam sie ze taki, narazie to
+#poczatek, potem jak juz beda sygnaly to bede nad nia pracowal
+func _on_CollisionDetector_body_entered(body):
+	health -= 10
+
+#tutaj jest funkcja ktora bedzie wyswietlala ekran porażki, grała jakis tam deathsound etc.
+func fail():
+	pass
+
+#odpowiada za nowa gre
+func new_game():
+	pass
+
+#ta funkcja odpowiada za sprawdzenie czy postac zyje
+func game_lasts():
+	if health <= 100:
+		lives -= 1
+	if lives == 0:
+		fail()
 	
 
